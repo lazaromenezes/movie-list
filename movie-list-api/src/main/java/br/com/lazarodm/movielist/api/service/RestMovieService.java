166cf58cd29f4b5e69ef1c70890e0cb29e81c32f
Lázaro Menezes <lazarodm@gmail.com>
2017-03-03 18:48:38 -0300

    Service WIP

--- movie-list-api/src/main/java/br/com/lazarodm/movielist/api/service/RestMovieService.java
+++ movie-list-api/src/main/java/br/com/lazarodm/movielist/api/service/RestMovieService.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class RestMovieService implements MovieService{
 
 	private List<String> movieStore;
-	
+
+	public RestMovieService(List<String> movieStore){
+		this.movieStore = movieStore;
+	}
+
 	public void addMovie(String movie) {
 		movieStore.add(movie);
 	}
--- movie-list-api/src/test/java/br/com/lazarodm/movielist/api/service/test/RestMovieServiceTest.java
+++ movie-list-api/src/test/java/br/com/lazarodm/movielist/api/service/test/RestMovieServiceTest.java
@@ -1,11 +1,23 @@
 package br.com.lazarodm.movielist.api.service.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import br.com.lazarodm.movielist.api.service.RestMovieService;
+
 public class RestMovieServiceTest {
+	private static final String MY_MOVIE = "My Movie";
+
 	@Test
-	public void test(){
-		Assert.assertTrue(false);
+	public void addMovieCanAddToMovieStore(){
+		List<String> movieStore = new ArrayList<String>();
+		RestMovieService service = new RestMovieService(movieStore);
+
+		service.addMovie(MY_MOVIE);
+
+		Assert.assertEquals(MY_MOVIE, movieStore.get(0));
 	}
 }
