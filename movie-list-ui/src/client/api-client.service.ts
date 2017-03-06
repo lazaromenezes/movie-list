12bede5f34fd3d8183516786d1270d1693789991
Lázaro Menezes <lazarodm@gmail.com>
2017-03-05 23:58:29 -0300

    Update resource URL into UI

--- movie-list-ui/src/client/api-client.service.ts
+++ movie-list-ui/src/client/api-client.service.ts
@@ -4,23 +4,21 @@ import 'rxjs/add/operator/map'
 
 @Injectable()
 export class MovieListApiClient {
+  private URL = 'http://localhost:8080/api/movie'
+
   constructor(private http: Http) {
   }
 
   list() {
-    const url = 'http://localhost:8080/movie-list-api/api/movie'
-
-    return this.http.get(url).map(response => response.json())
+    return this.http.get(this.URL).map(response => response.json())
   }
 
   save(movie: any) {
-    const url = 'http://localhost:8080/movie-list-api/api/movie'
-
     let headers = new Headers()
     headers.append('Content-Type', 'application/json')
     let options = new RequestOptions()
     options.headers = headers
 
-    return this.http.post(url, movie, options)
+    return this.http.post(this.URL, movie, options)
   }
 }
