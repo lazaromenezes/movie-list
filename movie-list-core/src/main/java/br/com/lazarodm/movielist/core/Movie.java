745f00edde52ac494dcd460947c59bdbbf6fcd62
Lázaro Menezes <lazarodm@gmail.com>
2020-08-07 20:51:23 -0300

    Fix error

--- movie-list-core/src/main/java/br/com/lazarodm/movielist/core/Movie.java
+++ movie-list-core/src/main/java/br/com/lazarodm/movielist/core/Movie.java
@@ -1,6 +1,7 @@
 package br.com.lazarodm.movielist.core;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Movie implements Serializable{
 	private static final long serialVersionUID = 1L;
